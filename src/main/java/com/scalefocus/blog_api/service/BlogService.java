85061b9b23b263aaec8df108b5f26aaf179c58db
Isil Acar <isil.acar@scalefocus.com>
2024-08-16 09:45:25 +0300

    adding blog update feature

--- src/main/java/com/scalefocus/blog_api/service/BlogService.java
+++ src/main/java/com/scalefocus/blog_api/service/BlogService.java
@@ -2,6 +2,7 @@ package com.scalefocus.blog_api.service;
 
 
 import com.scalefocus.blog_api.dto.BlogDto;
+import com.scalefocus.blog_api.request.BlogUpdateRequest;
 
 import java.util.List;
 
@@ -10,4 +11,6 @@ public interface BlogService {
     BlogDto createBlog(BlogDto blogDto);
 
     List<BlogDto> getAllBlogs();
+
+    BlogDto updateBlog(Long blogId, BlogUpdateRequest blogUpdateRequest);
 }
--- src/main/java/com/scalefocus/blog_api/service/impl/BlogServiceImpl.java
+++ src/main/java/com/scalefocus/blog_api/service/impl/BlogServiceImpl.java
@@ -3,11 +3,14 @@ package com.scalefocus.blog_api.service.impl;
 
 import com.scalefocus.blog_api.dto.BlogDto;
 import com.scalefocus.blog_api.entity.Blog;
+import com.scalefocus.blog_api.exception.ResourceNotFound;
 import com.scalefocus.blog_api.mapper.BlogMapper;
 import com.scalefocus.blog_api.repository.BlogRepository;
+import com.scalefocus.blog_api.request.BlogUpdateRequest;
 import com.scalefocus.blog_api.service.BlogService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 
 @Service
@@ -26,6 +29,19 @@ public class BlogServiceImpl implements BlogService {
     @Override
     public List<BlogDto> getAllBlogs() {
         return blogMapper.mapToBlogDtoList(blogRepository.findAll());
+    }
+
+    @Override
+    public BlogDto updateBlog(Long blogId, BlogUpdateRequest blogUpdateRequest) {
+        Blog blog = blogRepository.findById(blogId)
+                .orElseThrow(() -> new ResourceNotFound("Blog does not exist with id: " + blogId));
+
+        blog.setTitle(blogUpdateRequest.title());
+        blog.setText(blogUpdateRequest.text());
+
+        blogRepository.save(blog);
+
+        return blogMapper.mapToBlogDto(blog);
     }
 
 
