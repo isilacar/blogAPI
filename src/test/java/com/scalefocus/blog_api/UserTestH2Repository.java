4403b15bb6019fc88ebf72a1004d619c085aad27
Işıl Acar <isilacar@hotmail.com.tr>
2024-09-24 09:16:27 +0300

    adding test container for integration tests

--- src/test/java/com/scalefocus/blog_api/UserTestH2Repository.java
+++
@@ -1,7 +0,0 @@
-package com.scalefocus.blog_api;
-
-import com.scalefocus.blog_api.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserTestH2Repository extends JpaRepository<User, Long> {
-}
